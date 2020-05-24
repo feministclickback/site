433e18676f7671a7219cc293fa7fc1d518bbf4e0
Tina <feministclickback@mail36.net>
2020-05-24 18:23:37 +0200

    Set up assets pipeline: configure gulp pipe
    I'm using this process: https://htr3n.github.io/2018/07/minimal-hugo-workflow/
    
    Gulpfile configuration is from there. It processes scss-file in assets,
    puts it through preprocessing-plugins, puts output in `static` and
    watches for changes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,30 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const log = require('fancy-log');
 
-exports.default = defaultTask
+const sassSourceFile = 'assets/scss/main.scss';
+const outputFolder = 'static/css';
+const watchedResources = 'assets/scss/**/*';
+
+gulp.task('scss', function (done) {
+  gulp.src(sassSourceFile)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', function(err){
+      log.error(err.message);
+    }))
+    .pipe(postcss([autoprefixer, cssnano]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(outputFolder))
+    .on('end', done);
+});
+
+gulp.task('watch', gulp.series('scss', function (done) {
+  gulp.watch(watchedResources, gulp.parallel('scss'));
+  done();
+}));
+
+gulp.task('default', gulp.series('watch', function () {}));
