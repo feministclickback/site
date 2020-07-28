7e1468ca68226a1cf527c9a571bf00809bc98112
feministclickback <feministclickback@mail36.net>
2020-07-28 22:33:51 +0200

    Change icon to close when burger menu is opened

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,8 +1,12 @@
 function myFunction() {
   var x = document.getElementById("main-nav");
+  var y = document.getElementById("toggle");
   if (x.className === "main-nav") {
     x.className += " responsive";
+    y.src="/svgs/close.svg";
   } else {
     x.className = "main-nav";
+    y.src="/svgs/menu-toggle.svg";
   }
+
 }
