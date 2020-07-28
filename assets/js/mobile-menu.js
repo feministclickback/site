048a06cbc3169f3384c8a0333d9e33ab10e743cc
feministclickback <feministclickback@mail36.net>
2020-07-28 22:56:22 +0200

    Add turretcss classes to js so it wouldn't remove them onclick

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,11 +1,11 @@
 function myFunction() {
   var x = document.getElementById("main-nav");
   var y = document.getElementById("toggle");
-  if (x.className === "main-nav") {
-    x.className += " responsive";
+  if (x.className === "main-nav group group-xxl") {
+    x.className += " responsive group-column";
     y.src="/svgs/close.svg";
   } else {
-    x.className = "main-nav";
+    x.className = "main-nav group group-xxl";
     y.src="/svgs/menu-toggle.svg";
   }
 
